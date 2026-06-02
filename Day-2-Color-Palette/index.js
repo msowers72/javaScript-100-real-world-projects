898e6f3543bdf3a2376d9f45b039b2ebe2ec539d
Mike Sowers <msowers72@yahoo.com>
2026-06-02 16:06:21 -0400

    project complete

--- Day-2-Color-Palette/index.js
+++ Day-2-Color-Palette/index.js
@@ -10,13 +10,31 @@ paletteContainer.addEventListener('click', function (e) {
 
     navigator.clipboard
       .writeText(hexValue)
-      .then(() => showCopySuccess())
+      .then(() => showCopySuccess(e.target))
+      .catch((err) => console.log(err));
+  } else if (e.target.classList.contains('color')) {
+    const hexValue =
+      e.target.nextElementSibling.querySelector('.hex-value').textContent;
+    navigator.clipboard
+      .writeText(hexValue)
+      .then(() =>
+        showCopySuccess(e.target.nextElementSibling.querySelector('.copy-btn'))
+      )
       .catch((err) => console.log(err));
   }
 });
 
-function showCopySuccess() {
-  
+function showCopySuccess(element) {
+  element.classList.remove('far', 'fa-copy');
+  element.classList.add('fas', 'fa-check');
+
+  element.style.color = '#48bb78';
+
+  setTimeout(() => {
+    element.classList.remove('fas', 'fa-check');
+    element.classList.add('far', 'fa-copy');
+    element.style.color = '';
+  }, 1500);
 }
 
 function generatePalette() {
